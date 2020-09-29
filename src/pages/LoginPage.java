c09f80bcce12197eeb84d7ce35b84c1191a1ed39
Darko Stefanovic <darkost0502@gmail.com>
2020-09-30 00:58:38 +0200

    Create setup and finish LoginPage

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,9 +1,46 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasicPage {
+	
+	public LoginPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+		
+	}
+
+	
+	
+	// webElements locator
+	public WebElement loginButton() {
+		return this.driver.findElement(By.xpath("//*[text()='Login']"));
+	}
+
+	public WebElement getUserName() {
+		return this.driver.findElement(By.name("username"));
+
+	}
+
+	public WebElement getPasword() {
+		return this.driver.findElement(By.name("password"));
+
+	}
+
+	public WebElement getLoginBtn() {
+		return this.driver.findElement(By.name("btn_submit"));
+	}
+
+	// Login method
+	public void Login(String userid, String password) {
+		getUserName().clear();
+		getUserName().sendKeys(userid);
+		getPasword().clear();
+		getPasword().sendKeys(password);
+		getLoginBtn().click();
+	}
 
 }
-//Login Page:
-//get metode za sve potrebne elemente
-//metodu koja prijavljuje korisnika na sistem -
-//kao parametri se prosleđuju imejl i lozinka
