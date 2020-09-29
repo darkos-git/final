7ac2f865723d042e1e54f325b2faf731de6c5028
Darko Stefanovic <darkost0502@gmail.com>
2020-09-30 00:55:46 +0200

    Update Basic test and repair after method screenshot

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -18,41 +18,34 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 public abstract class BasicTest {
-	protected String baseUrl = "http://demo.yo-meals.com";
-	protected String userId = "customer@dummyid.com";
-	protected String userPw = "12345678a";
 	protected WebDriver driver;
 	protected WebDriverWait wait;
 	protected JavascriptExecutor js;
-
-	public BasicTest(String baseUrl, String userId, String userPw, WebDriver driver, WebDriverWait wait,
-			JavascriptExecutor js) {
-		this.baseUrl = baseUrl;
-		this.userId = userId;
-		this.userPw = userPw;
-		this.driver = driver;
-		this.wait = wait;
-		this.js = js;
-	}
+	protected String baseUrl = "http://demo.yo-meals.com";
+	protected String userId = "customer@dummyid.com";
+	protected String userPw = "12345678a";
 
 	@BeforeClass
 	public void beforeClass() {
+
 		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
 		this.driver = new ChromeDriver();
+		this.js = (JavascriptExecutor) driver;
 
 		this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		this.driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		// WebDriverWait wait = new WebDriverWait(driver, 10);
+		this.driver.manage().window().maximize();
+
 	}
 
 	// if test fail this method make screenshot
 	@AfterMethod
 	public void afterTest(ITestResult result) throws Exception {
-		String testTime = new SimpleDateFormat("yyyyMMddHHmmss'.txt'").format(new Date());
+		String testTime = new SimpleDateFormat("yyyyMMddHHmm'.txt'").format(new Date());
 		if (result.getStatus() == ITestResult.FAILURE) {
 			TakesScreenshot ts = (TakesScreenshot) driver;
 			File ss = ts.getScreenshotAs(OutputType.FILE);
-			FileUtils.copyDirectory(ss, new File("./screenshots/" + testTime + ".png"));
+			FileUtils.copyFile(ss, new File("C:\\Users\\bice\\Desktop\\FINAL PROJECT\\final\\screenshots\\" + testTime + ".png"));
 		}
 		this.driver.manage().deleteAllCookies();
 	}
